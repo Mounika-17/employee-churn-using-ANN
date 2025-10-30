9e815eef6cecad1d3530162755a0e13e7bba84e2
Mounika-17 <maradana.mounika17@gmail.com>
2025-10-30 17:23:36 +0530

    updated model.py file

--- src/components/model.py
+++ src/components/model.py
@@ -4,10 +4,10 @@ from tensorflow.keras.optimizers import Adam
 
 def build_ann_model(input_dim):
     model = Sequential([
-        Dense(64, activation='relu', input_shape=(input_dim,)),
-        Dropout(0.3),
-        Dense(32, activation='relu'),
-        Dense(1, activation='sigmoid')  # For binary classification
+        Dense(64, activation='relu', input_shape=(input_dim,)), # THis represents the input layer and 64 neurons in the hidden layer and activation function is relu
+        Dropout(0.3), # This represents the dropout layer means drop 30% of the neurons in the hidden layer
+        Dense(32, activation='relu'), # This represents the hidden layer 2 and 32 neurons are present in this hidden layer
+        Dense(1, activation='sigmoid')  # For binary classification sigmoid activation function is used and this is the output layer with 1 neuron
     ])
     
     model.compile(
